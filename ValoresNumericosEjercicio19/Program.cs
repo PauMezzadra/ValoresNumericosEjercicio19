6c5df9059b85874a8bc5f59379d426963ebfb9af
Pau Mezzadra <paumezzadra@hotmail.com>
2021-03-30 13:09:21 -0300

    Add project files.

---
+++ ValoresNumericosEjercicio19/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValoresNumericosEjercicio19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal numeroDecimal;
+            Console.WriteLine("Ingrese un numero con varios decimales");
+            var numero = Console.ReadLine();
+            if (!decimal.TryParse(numero, out numeroDecimal))
+            {
+                Console.WriteLine("Debe ingresar un número decimal");
+            }
+            else
+            {
+                Console.WriteLine($"El numero que ud. ingresó es: {numero}, y " +
+                    $"redondeado a dos decimales es: {Math.Floor(numeroDecimal)}");
+
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
